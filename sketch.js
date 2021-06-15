35e1305849fd15209101619f7b210401dbd59f13
Domiziano Scarcelli <scarcelli.d@outlook.com>
2021-06-15 18:16:33 +0200

    Animations and stuff

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ class Node {
 	}
 }
 let start = new Node(500, 500, undefined, 0);
-let end = new Node(1400, 700);
+let end = new Node(10, 10);
 
 var nodes = [];
 
@@ -27,12 +27,32 @@ for (var y = 0; y < 80; y++) {
 
 function setup() {
 	createCanvas(1500, 800);
-
 	fill(0, 200, 0);
 	rect(start.x, start.y, 10);
 	fill(200, 0, 0);
 	rect(end.x, end.y, 10);
 	fill(50, 50, 50);
+}
+
+function execute() {
+	dijkstra();
+	showPath();
+}
+
+function showPath() {
+	setTimeout(() => {
+		fill(0, 0, 200);
+		let node = end;
+		if (node.prec != undefined) {
+			while (node != undefined) {
+				node = node.prec;
+				if (!(node.x == start.x && node.y == start.y)) square(node.x, node.y, 10);
+			}
+		}
+	});
+}
+
+function dijkstra() {
 	while (nodes.length != 0) {
 		var u = nodes.reduce((prev, curr) => (prev.dist < curr.dist ? prev : curr));
 		if (u.prec == end) nodes = [];
@@ -41,112 +61,12 @@ function setup() {
 			if (!(v.x == end.x && v.y == end.y)) {
 				setTimeout(() => {
 					square(v.x, v.y, 10);
-				}, 10);
+				}, 5);
 			}
-
 			let alt = u.dist + 1;
 			if (alt < v.dist) {
 				v.dist = alt;
 				v.prec = u;
-			}
-		});
-	}
-	fill(100, 40, 20);
-	let node = end;
-	if (node.prec != undefined) {
-		while (node != undefined) {
-			square(node.x, node.y, 10);
-			node = node.prec;
-		}
-	}
-}
-
-async function drawPath() {}
-
-function draw() {}
-
-// function draw() {
-// 	fill(0, 200, 0);
-// 	rect(start.x, start.y, 10);
-// 	fill(200, 0, 0);
-// 	rect(end.x, end.y, 10);
-// 	fill(0, 200, 0);
-// 	let visited = [];
-// 	while (nodes.length != 0) {
-// 		var u = nodes.reduce((prev, curr) => (prev.dist < curr.dist ? prev : curr));
-// 		if (u.prec == end) nodes = [];
-// 		nodes = nodes.filter((elem) => elem != u);
-// 		visited.push(u);
-// 		adjacent(u).forEach((v) => {
-// 			if (!visited.includes(v)) {
-// 				setTimeout(() => {
-// 					square(v.x, v.y, 10);
-// 				}, 20);
-
-// 				let alt = u.dist + 1;
-// 				if (alt < v.dist) {
-// 					v.dist = alt;
-// 					v.prec = u;
-// 				}
-// 			}
-// 		});
-// 	}
-// 	fill(50);
-// 	let path = [];
-// 	let node = end;
-// 	if (node.prec != undefined) {
-// 		while (node != undefined) {
-// 			path.push(node);
-// 			square(node.x, node.y, 10);
-// 			node = node.prec;
-// 		}
-// 	}
-// 	console.log(path);
-// }
-
-// function mouseDragged() {
-// 	dim = 10;
-// 	// fill(51);
-// 	x = mouseX - (mouseX % dim);
-// 	y = mouseY - (mouseY % dim);
-// 	animate(x, y, dim);
-// }
-
-// function mouseClicked() {
-// 	dim = 10;
-// 	// fill(51);
-// 	x = mouseX - (mouseX % dim);
-// 	y = mouseY - (mouseY % dim);
-// 	animate(x, y, dim);
-// }
-
-// function animate(x, y, dim) {
-// 	for (let i = 0; i < dim; i++) {
-// 		setTimeout(() => {
-// 			fill(51);
-// 			square(x, y, i);
-// 		}, i * 2);
-// 	}
-// }
-
-function shortestPath() {
-	let visited = [];
-	while (nodes.length != 0) {
-		var u = nodes.reduce((prev, curr) => (prev.dist < curr.dist ? prev : curr));
-		if (u.prec == end) nodes = [];
-		nodes = nodes.filter((elem) => elem != u);
-		visited.push(u);
-		adjacent(u).forEach((v) => {
-			if (!visited.includes(v)) {
-				setTimeout(() => {
-					square(v.x, v.y, 10);
-				}, 20);
-
-				let alt = u.dist + 1;
-				if (alt < v.dist) {
-					v.dist = alt;
-					v.prec = u;
-				}
 			}
 		});
 	}
